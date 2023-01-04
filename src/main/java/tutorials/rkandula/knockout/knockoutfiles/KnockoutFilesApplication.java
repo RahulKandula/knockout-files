42da79bc68163a28e561fdc871fe0d6feaeca45c
Rahul Kandula <rkumar@rpmpcg.com>
2023-01-04 12:31:45 +0530

    done so far

---
+++ src/main/java/tutorials/rkandula/knockout/knockoutfiles/KnockoutFilesApplication.java
@@ -0,0 +1,13 @@
+package tutorials.rkandula.knockout.knockoutfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockoutFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockoutFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tutorials/rkandula/knockout/knockoutfiles/KnockoutFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorials.rkandula.knockout.knockoutfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockoutFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
